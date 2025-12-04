e0fdc138bc27bcff8962f954e2a4643f20064ef6
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-12-04 12:08:45 +0900

    implement fizzbuzz

--- fizzbuzz/app/Main.hs
+++ fizzbuzz/app/Main.hs
@@ -1,4 +1,13 @@
 module Main where
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = mapM_ (putStrLn . fizzbuzz) [1..16]
+
+fizzbuzz :: Int -> String
+fizzbuzz n
+  | n <= 0 = "error"
+  | n `mod` 15 == 0 = "FizzBuzz"
+  | n `mod` 3 == 0 = "Fizz"
+  | n `mod` 5 == 0 = "Buzz"
+  | otherwise = show n
+
